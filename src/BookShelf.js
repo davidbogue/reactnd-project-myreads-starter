31eafd27c1a7b2b96e90fa7aa00f2a2a46953d98
davidbogue <davebogue@gmail.com>
2017-08-02 21:41:43 -0500

    feat: Implemented routing, API, and bookshelf component
    Implemented routing using react-router-dom. Used BooksAPI to load books from external source. Created bookshelf component and began to display books dynamically.

---
+++ src/BookShelf.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import sortBy from 'sort-by'
+
+class BookShelf extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    shelfName: PropTypes.string.isRequired,
+    shelfValue: PropTypes.string.isRequired
+    // onUpdateBook: PropTypes.func.isRequired
+  }
+
+  render() {
+    const { books, shelfName, shelfValue } = this.props
+
+    books.sort(sortBy('title'))
+
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelfName}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.filter((b) => b.shelf === shelfValue).map((book) => (
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                    <div className="book-shelf-changer">
+                      <select>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">
+                    {book.authors.map((author) => (
+                      <div key={author}>{author}</div>
+                    ))}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BookShelf
